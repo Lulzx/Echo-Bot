d402fe5c5eeb2260b95f4e26329dae3264f0a5d9
Lulzx <wiserishabh@gmail.com>
2017-10-23 11:03:50 +0530

    LMAO

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 # It echoes any incoming text messages.
 
 print ("Python Bot By Lulzx")
-print ("Kosgu Bot has been started.")
+print ("Lulzx Bot has been started.")
 
 import telebot
 
@@ -13,18 +13,12 @@ bot = telebot.TeleBot(API_TOKEN)
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am Kos Goo Bot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!
-Created By @ArashEmp\
+    bot.reply_to(message, """\Fuck off! 😒\
 """)
 
 @bot.message_handler(commands=['credits', 'about'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am KosGu BOT
-An Multi Purpose Telegram Bot Written In Python By @ArashEmp
-Licenced Under Gnu General v.3 License!\
+    bot.reply_to(message, """\Fuck off! 😒\
 """)
 
 
